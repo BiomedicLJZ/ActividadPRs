600ce716e165386e7f67dabfa443290fc227521a
Eduardo Flores <eduardo.flores@uniat.edu.mx>
2026-07-05 23:22:31 -0700

    Casi funciona. La matriz que entrega está transpuesta.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
 int main() {
-	const int filas = 3;
-	const int valorcompartido = 3;
-	const int columnas = 3;
+	int filas;
+	int valorcompartido;
+	int columnas;
 	
-	/*
 	std::cout << "Escribir el número de filas de la primera matriz: " << std::endl;
 	std::cin >> filas;
 	std::cout << "Escribir el número de columnas de la primera matriz: " << std::endl;
@@ -13,54 +12,54 @@ int main() {
 	std::cout << "La segunda matriz debe tener " << valorcompartido << " filas para poder hacer la multiplicación." << std::endl;
 	std::cout << "Escribir el número de columnas de la segunda matriz: " << std::endl;
 	std::cin >> columnas;
-	*/
 	
-	auto* m1 = new float[filas][valorcompartido];
-	auto* m2 = new float[valorcompartido][columnas];
+	auto* m1 = static_cast<float*>(calloc((filas * valorcompartido), sizeof(float)));
+	auto* m2 = static_cast<float*>(calloc((valorcompartido * columnas), sizeof(float)));
 	
 	std::cout << "PRIMERA MATRIZ" << std::endl;
-	for (int j = 0; j < valorcompartido; ++j) {	
-		for (int i = 0; i < filas; ++i) {
-			std::cout << "Fila " << (i + 1) << ", columna " << (j + 1) << ": " << std::endl;
-			std::cin >> m1[i][j];
+	for (int j = 0; j < filas; ++j) {	
+		for (int i = 0; i < valorcompartido; ++i) {
+			std::cout << "Fila " << (j + 1) << ", columna " << (i + 1) << ": " << std::endl;
+			std::cin >> *(m1 + (i * filas) + j);
 		}
 	}
 	
 	std::cout << "SEGUNDA MATRIZ" << std::endl;
-	for (int j = 0; j < columnas; ++j) {	
-		for (int i = 0; i < valorcompartido; ++i) {
-			std::cout << "Fila " << (i + 1) << ", columna " << (j + 1) << ": " << std::endl;
-			std::cin >> m1[i][j];
+	for (int j = 0; j < valorcompartido; ++j) {	
+		for (int i = 0; i < columnas; ++i) {
+			std::cout << "Fila " << (j + 1) << ", columna " << (i + 1) << ": " << std::endl;
+			std::cin >> *(m2 + (i * valorcompartido) + j);
 		}
 	}
 	
-	auto* m3 = new float[filas][columnas];
+	auto* m3 = static_cast<float*>(calloc((filas * columnas), sizeof(float)));
 	
-	for (int j = 0; j < columnas; ++j) {	
-		for (int i = 0; i < filas; ++i) {
-			m3[i][j] = 0;
-		}
+	for (int i = 0; i < (filas * columnas); ++i) {
+		*(m3 + i) = 0;
 	}
 	
-	for (int k = 0; k < valorcompartido; ++k) {	
-		for (int j = 0; j < columnas; ++j) {	
-			for (int i = 0; i < filas; ++i) {
-				m3[i][j] += m1[i][k] * m2[k][j];
+	for (int k = 0; k < valorcompartido; ++k) {
+		std::cout << "===================" << std::endl;
+		for (int j = 0; j < filas; ++j) {
+			for (int i = 0; i < columnas; ++i) {
+				std::cout << (*(m3 + (j * filas) + i)) << " >> ";
+				*(m3 + (j * filas) + i) += (*(m1 + (k * filas) + i)) * (*(m2 + (j * valorcompartido) + k));
+				std::cout << (*(m3 + (j * filas) + i)) << " (" << (*(m1 + (k * filas) + i)) << " * " << (*(m2 + (j * valorcompartido) + k)) << ")" << std::endl;
 			}
 		}
 	}
 	
 	std::cout << "La matriz resultante es: " << std::endl;
-	for (int j = 0; j < columnas; ++j) {	
-		for (int i = 0; i < filas; ++i) {
-			std::cout << "	" << m3[i][j];
+	for (int i = 0; i < (filas * columnas); ++i) {	
+		std::cout << "	" << *(m3 + i);
+		if (i % filas == (filas - 1)) {
+			std::cout << std::endl;
 		}
-		std::cout << std::endl;
 	}
 	
-	delete[] m1;
-	delete[] m2;
-	delete[] m3;
+	free(m1);
+	free(m2);
+	free(m3);
 	
 	return 0;
 }
